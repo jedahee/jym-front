20411e808bfbfb3cac5898234f24146b36ea26a2
jedahee <jedahee02@gmail.com>
2022-12-13 14:44:12 +0100

    Deploy local

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
-  API_URL: 'https://jym-back.herokuapp.com/api/',
-  URL: 'https://jym-back.herokuapp.com/'
+  //API_URL: 'https://jym-back.herokuapp.com/api/',
+  //URL: 'https://jym-back.herokuapp.com/'
+  API_URL: 'http://127.0.0.1:8000/api/',
+  URL: 'http://127.0.0.1:8000'
 };
